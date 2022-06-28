23fcaed6af221f48409131737fa5972f5b86270a
CarolinaShlain123 <caroshlain@gmail.com>
2022-06-28 13:31:40 -0300

    modifique las fotos y agregue el footer

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express= require ('express');
 const app= express();
 const path= require ('path');
-const port= process.env.PORT || 3000;
+const port= process.env.PORT || 1000;
 
 app.listen(port, ()=> console.log("Listening on http://localhost:" + port));
 
